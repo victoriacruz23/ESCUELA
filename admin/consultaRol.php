c9428b12eff201aea44b31726a4e589e5148f915
victoria <cruzdelossantos.yaremivictoria@utacapulco.edu.mx>
2022-07-06 23:14:38 -0500

    Consultas alumnos

--- admin/consultaRol.php
+++ admin/consultaRol.php
@@ -48,7 +48,7 @@ if(isset($_SESSION["usuario"])){
                         </tr>
                     </thead>
                     <tbody class="table-primary">
-                    <?php
+                        <?php
                     require_once("../databases/conexion.php");
                     $roles = $conexion -> query("SELECT * FROM rol");
                     if($roles->num_rows > 0){
@@ -58,18 +58,36 @@ if(isset($_SESSION["usuario"])){
                             <th scope="row"><?php echo $t["RolId"]; ?></th>
                             <td><?php echo $t["RolNombre"]; ?></td>
                             <div style="justify-content: center;">
-                                <td><a href="../databases/EliminarRol.php?eliminar=<?php echo $t['RolId']; ?>" class="btn btn-danger">Eliminar</a></td>
+                                <td><a href="../databases/EliminarRol.php?eliminar=<?php echo $t['RolId']; ?>"
+                                        class="btn btn-danger">Eliminar</a></td>
                             </div>
                         </tr>
-                <?php  
+                        <?php  
                       }
                     }else{
                         echo "No existen roles";
                     }
-                    ?>    
-                        
+                    ?>
+
                     </tbody>
                 </table>
+                <div class="mt-4">
+                    <center>
+                        <a style="margin-right: 20px;" href="registroRol.php" class="text-dark"> <svg
+                                xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+                            </svg></a>
+                        <!-- <a style="margin-left: 20px;" href="eliminarusuario.php" class="text-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                            </svg>
+                        </a> -->
+                    </center>
+                </div>
             </div>
         </div>
     </div>
