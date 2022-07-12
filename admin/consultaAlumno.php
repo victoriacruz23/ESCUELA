c29234640c81e0283bd4578d3b555af4cfda1f31
victoria <cruzdelossantos.yaremivictoria@utacapulco.edu.mx>
2022-07-12 00:35:21 -0500

    modificacion

--- admin/consultaAlumno.php
+++ admin/consultaAlumno.php
@@ -77,15 +77,15 @@ if(isset($_SESSION["usuario"])){
 
                         </tbody>
                     </table>
-                    <!-- <div class="mt-4">
+                    <div class="mt-4">
                         <center>
-                            <a style="margin-right: 20px;" href="registrodeusuario.php" class="text-dark"> <svg
+                            <a style="margin-right: 20px;" href="registroalumno.php" class="text-dark"> <svg
                                     xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
                                     class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                                     <path
                                         d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
                                 </svg></a>
-                            <a style="margin-left: 20px;" href="eliminarusuario.php" class="text-dark">
+                            <a style="margin-left: 20px;" href="eliminaralumnos.php" class="text-dark">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
                                     class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                                     <path
@@ -93,7 +93,7 @@ if(isset($_SESSION["usuario"])){
                                 </svg>
                             </a>
                         </center>
-                    </div> -->
+                    </div> 
                 </div>
             </div>
         </div>
